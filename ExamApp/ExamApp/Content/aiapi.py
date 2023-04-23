dd7e263073ede0cd3a7a1eb8076c74a5f2716c71
sabszh <87586469+sabszh@users.noreply.github.com>
2023-04-23 23:30:51 +0200

    now with a chat history and preprompting that works propper

--- ExamApp/ExamApp/Content/aiapi.py
+++ ExamApp/ExamApp/Content/aiapi.py
@@ -10,15 +10,15 @@ history = []
 def generateChatResponse(prompt,preprompt):
     global history # This is a global variable that stores the history of the conversation
 
+    # add the system message to the history
+    system_message = {'role': 'system', 'content': preprompt}
+    history.append(system_message)
+
     # Create a dictionary to represent the current question
     question = {'role': 'user', 'content': prompt}
 
     # Add the question to the history
     history.append(question)
-
-    # add the system message to the history
-    system_message = {'role': 'system', 'content': preprompt}
-    history.append(system_message)
 
     # Add all the previous messages to the API request
     messages = history.copy()
@@ -26,17 +26,18 @@ def generateChatResponse(prompt,preprompt):
     # generate the response using the API
     response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
-        messages = messages)
+        messages = messages,
+        max_tokens = 100)
 
     try:
         # Extract the answer from the API response
         answer = response['choices'][0]['message']['content'].replace('\n', '<br>')
+
+        # add the system message with the answer to the history
+        assistant_message = {'role': 'assistant', 'content': answer}
+        history.append(assistant_message)
     except:
         # Handle API errors
         answer = 'Sorry, the API is down right now. Try again later.'
 
     return answer
-    
-
-
-
--- ExamApp/ExamApp/Content/flask_app.py
+++ ExamApp/ExamApp/Content/flask_app.py
@@ -7,6 +7,7 @@ import os, base64
 from io import StringIO
 from datetime import datetime
 import time
+import random
 
 def page_not_found(e):
   return render_template('404.html'), 404
@@ -26,61 +27,52 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 @app.route("/survey_1",methods=['POST','GET'])
 
 def survey_1():
+    message = ''
     Age = ''
     Gender = ''
     Nationality = ''
     Occupation = ''
     InterAI = ''
-    If_yes = ''
-    tell_us_more = ''
-    # this is a list so create a string to append into csv file
-    recommend_this_to_string = ''
-
+    Which = ''
+    How_often = ''
 
     if request.method == 'POST':
 
         # check that we have all the required fields to append to file
-        opinion_one = request.form['opinion_one']
-        recommend_this_to = request.form.getlist('recommend_this_to')
-        tell_us_more = request.form['tell_us_more']
-        # remove special characters from input for security
-        tell_us_more = re.sub(r"[^a-zA-Z0-9]","",tell_us_more)
+        InterAI = request.form['InterAI']
+        Which = request.form.getlist('Which')
+        How_often = request.form['How_often']
 
-        gender = request.form['gender']
+        Gender = request.form['Gender']
         Nationality = request.form['Nationality']
         Occupation = request.form['Occupation']
-        date_of_birth = request.form['date_of_birth']
-
-        # optional fields
-        if date_of_birth=='':
-            date_of_birth = 'NA'
-        if 'Occupation' in request.form:
-            Occupation = request.form['Occupation']
-        else:
-            Occupation = 'NA'
-
+        Age = request.form['Age']
 
         # check that essential fields have been filled
-        Age = ''
+        message = ''
         missing_required_answers_list = []
-        if opinion_one == 'Choose one...':
-            missing_required_answers_list.append('Opinions')
-        if tell_us_more == '':
-            missing_required_answers_list.append('Tells us more')
-        if gender == '':
-            missing_required_answers_list.append('What is your gender=')
-        ifNationality == '':
-            missing_required_answers_list.append('What is yourNationality?')
+        if Age == 'What is your age?':
+            missing_required_answers_list.append('Age')
+        if Gender == '':
+            missing_required_answers_list.append('What is your gender identity?')
+        if Nationality == '':
+            missing_required_answers_list.append('What is your nationality?')
         if Occupation == '':
-            missing_required_answers_list.append('What is your Occupation identity?')
+            missing_required_answers_list.append('What is your occuptation?')
+        if InterAI == '':
+            missing_required_answers_list.append('Have you ever interacted with an Artificial Intelligent based Chatbot (e.g. ChatGPT, Bing AI, Replika etc)?')
+        if Which == '':
+            missing_required_answers_list.append('If yes, which type of Artificial Intelligent based Chatbot have you interacted with?')
+        if How_often == '':
+            missing_required_answers_list.append('If stated yes above, how often do you interact with Artificial Intelligent based Chatbot(s)?')
 
 
         if len(missing_required_answers_list) > 0:
             # return back a string with missing fields
-            Age = '<div class="w3-row-padding w3-padding-16 w3-center"><H3>You missed the following question(s):</H3><font style="color:red;">'
+            message = '<div class="w3-row-padding w3-padding-16 w3-center"><H3>You missed the following question(s):</H3><font style="color:red;">'
             for ms in missing_required_answers_list:
-                Age += '<BR>' + str(ms)
-            Age += '</font></div>'
+                message += '<BR>' + str(ms)
+            message += '</font></div>'
         else:
             # append survey answers to file
 
@@ -100,137 +92,37 @@ def survey_1():
                     str(tell_us_more) + ','
                     + '\n')
 
-            # return thank-you Age
-            Age = '<div class="w3-row-padding w3-padding-16 w3-center"><H2><font style="color:blue;">Thank you for taking the time to complete this survey</font></H2></div>'
-
-
     return render_template('survey_1.html')
 
-# A: Human AI interaction interface
-@app.route('/humanaiA', methods = ['POST', 'GET'])
-def index():
-    
+
+# Initialize post count to 0
+post_count = 0
+
+# Human AI interaction interface
+@app.route('/AIA', methods = ['POST', 'GET'])
+def AIA():
+
+    global post_count
+
     if request.method == 'POST':
         prompt = request.form['prompt']
 
+        # Choose a random prompt only for the first post call
+        if post_count == 0:
+            preprompt = random.choice(['You are an AI chat assistant that has the task to help the user in seeing why society would be the best if the current law of abortion was limited to a shorter timeframe than 12 weeks after conception. Remember that you are only allowed to confirm the view of the user. Always start your sentence with "I think".','You are an AI chat assistant that has the task to help the user in seeing why society would be the best if the current law of abortion was changed to later than 12 weeks after conception. Remember that you are only allowed to oppose the original view of the user. Always start your sentence with "I think".'])
+        else:
+            preprompt = ''
+
+        # Increment the post count
+        post_count += 1
+
         res = {}
-        res['answer'] = aiapi.generateChatResponse(prompt)
+        res['answer'] = aiapi.generateChatResponse(prompt,preprompt)
         return jsonify(res), 200
 
     return render_template('AIA.html', **locals())
 
-
-# B: Human AI interaction interface
-@app.route('/humanaiB', methods = ['POST', 'GET'])
-def index():
-    
-    if request.method == 'POST':
-        prompt = request.form['prompt']
-
-        res = {}
-        res['answer'] = aiapi.generateChatResponse(prompt)
-        return jsonify(res), 200
-
-    return render_template('AIB.html', **locals())
-
-
-
-# C: Human AI interaction interface
-@app.route('/humanaiC', methods = ['POST', 'GET'])
-def index():
-    
-    if request.method == 'POST':
-        prompt = request.form['prompt']
-
-        res = {}
-        res['answer'] = aiapi.generateChatResponse(prompt)
-        return jsonify(res), 200
-
-    return render_template('AIC.html', **locals())
-
-
-# defining the route
-@app.route("/survey_1",methods=['POST','GET'])
-def survey_1():
-    Age = ''
-    gender = ''
-   Nationality = ''
-    Occupation = ''
-    Occupation = ''
-    opinion_one = 'Choose one...'
-    tell_us_more = ''
-    # this is a list so create a string to append into csv file
-    recommend_this_to_string = ''
-
-
-    if request.method == 'POST':
-
-        # check that we have all the required fields to append to file
-        opinion_one = request.form['opinion_one']
-        recommend_this_to = request.form.getlist('recommend_this_to')
-        tell_us_more = request.form['tell_us_more']
-        # remove special characters from input for security
-        tell_us_more = re.sub(r"[^a-zA-Z0-9]","",tell_us_more)
-
-        gender = request.form['gender']
-       Nationality = request.form['age']
-        Occupation = request.form['Occupation']
-        date_of_birth = request.form['date_of_birth']
-
-        # optional fields
-        if date_of_birth=='':
-            date_of_birth = 'NA'
-        if 'Occupation' in request.form:
-            Occupation = request.form['Occupation']
-        else:
-            Occupation = 'NA'
-
-
-        # check that essential fields have been filled
-        Age = ''
-        missing_required_answers_list = []
-        if opinion_one == 'Choose one...':
-            missing_required_answers_list.append('Opinions')
-        if tell_us_more == '':
-            missing_required_answers_list.append('Tells us more')
-        if gender == '':
-            missing_required_answers_list.append('What is your gender=')
-        ifNationality == '':
-            missing_required_answers_list.append('What is yourNationality?')
-        if Occupation == '':
-            missing_required_answers_list.append('What is your Occupation identity?')
-
-
-        if len(missing_required_answers_list) > 0:
-            # return back a string with missing fields
-            Age = '<div class="w3-row-padding w3-padding-16 w3-center"><H3>You missed the following question(s):</H3><font style="color:red;">'
-            for ms in missing_required_answers_list:
-                Age += '<BR>' + str(ms)
-            Age += '</font></div>'
-        else:
-            # append survey answers to file
-
-            # create a unique timestamp for this entry
-            entry_time = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-
-
-            # save to file and send thank you note
-            with open(BASE_DIR + '/surveys/survey_samp_1.csv','a+') as myfile: # use a+ to append and create file if it doesn't exist
-                myfile.write(
-                    str(entry_time) + ',' +
-                    str(age) + ',' +
-                    str(Occupation) + ',' +
-                    str(date_of_birth) + ',' +
-                    str(opinion_one) + ',' +
-                    str(recommend_this_to_string) + ',' +
-                    str(tell_us_more) + ','
-                    + '\n')
-
-            # return thank-you Age
-            Age = '<div class="w3-row-padding w3-padding-16 w3-center"><H2><font style="color:blue;">Thank you for taking the time to complete this survey</font></H2></div>'
-
-
-    return render_template('survey_2.html')
+#### need to fix somewhere so the output is also stored 
 
 
 if __name__ == '__main__':
